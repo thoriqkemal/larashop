cc369b6ab5b6e55b2598f960885f4a5e3abbe4f7
thoriqkemal <thoriqekemal@gmail.com>
2024-03-24 15:16:24 +0700

    filter use by email

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,9 +14,16 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $users = User::paginate(10);
+
+        $filterKeyword = $request->get('keyword');
+
+        if($filterKeyword) {
+            $users = User::where('email', 'LIKE', "%$filterKeyword%")->paginate(10);
+        }
+
         return view('users.index', ['users' => $users]);
     }
 
