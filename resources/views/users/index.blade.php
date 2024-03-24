8968be70700d8aa83ff6ba6c2716c0367af9d2c0
thoriqkemal <thoriqekemal@gmail.com>
2024-03-24 16:37:17 +0700

    Users Fix

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -82,5 +82,12 @@
         </tr>
         @endforeach
     </tbody>
+    <tfoot>
+        <tr>
+            <td colspan=10>
+                {{ $users->appends(Request::all())->links() }}
+            </td>
+        </tr>
+    </tfoot>
 </table>
 @endsection
