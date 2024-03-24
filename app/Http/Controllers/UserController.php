fad639792938601a4d5c940687c16df9888ea3d0
thoriqkemal <thoriqekemal@gmail.com>
2024-03-24 13:23:17 +0700

    create users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -34,7 +36,22 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $new_user = new User();
+        $new_user->name = $request->get('name');
+        $new_user->username = $request->get('username');
+        $new_user->roles = json_encode($request->get('roles'));
+        $new_user->address = $request->get('address');
+        $new_user->phone = $request->get('phone');
+        $new_user->email = $request->get('email');
+        $new_user->password = Hash::make($request->get('password'));
+
+        if($request->file('avatar')) {
+            $file = $request->file('avatar')->store('avatars', 'public');
+            $new_user->avatar = $file;
+        }
+
+        $new_user->save();
+        return redirect()->route('users.create')->with('status', 'User successfully created');
     }
 
     /**
