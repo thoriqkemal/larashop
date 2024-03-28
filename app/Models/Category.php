fe4cc1338dace48fd204c920a1a3cf8d3775e3ed
thoriqkemal <thoriqekemal@gmail.com>
2024-03-28 14:43:30 +0700

    book-category model relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Book;
 
 class Category extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function books() {
+        return $this->belongsToMany('Book');
+    }
 }
