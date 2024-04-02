87641acbada5010e8f002e60898b75104bf9e19c
thoriqkemal <thoriqekemal@gmail.com>
2024-04-02 11:23:19 +0700

    list orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -13,7 +14,9 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $orders = Order::with('user')->with('books')->paginate(10);
+
+        return view('orders.index', ['orders' => $orders]);
     }
 
     /**
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    use HasFactory;
 
     public function user() {
         return $this->belongsTo('App\Models\User');
@@ -15,5 +13,15 @@ class Order extends Model
 
     public function books() {
         return $this->belongsToMany('App\Models\Book')->withPivot('quantity');
+    }
+
+    public function getTotalQuantityAttribute() {
+        $total_quantity = 0;
+
+        foreach ($this->books() as $book) {
+            $total_quantity += $book->pivot->quantity;
+        }
+
+        return $total_quantity;
     }
 }
